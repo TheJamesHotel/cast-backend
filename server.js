d10daa504d431826b9b394a0a31050aae9235590
Niels Bakker <bakkerniels@me.com>
2026-04-10 13:39:52 +0200

    Temporary minimal server for Render test

--- server.js
+++ server.js
@@ -1,29 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import "./db/database.js";
-
-import tvRoutes from "./routes/tvRoutes.js";
-import pairRoutes from "./routes/pairRoutes.js";
-import webRoutes from "./routes/webRoutes.js";
-import displayRoutes from "./routes/displayRoutes.js";
-
-dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("ok");
+});
 
 app.get("/health", (req, res) => {
   res.json({ ok: true });
 });
-
-app.use("/api/tv", tvRoutes);
-app.use("/api/pair", pairRoutes);
-app.use("/", webRoutes);
-app.use("/", displayRoutes);
 
 const PORT = process.env.PORT || 3000;
 
